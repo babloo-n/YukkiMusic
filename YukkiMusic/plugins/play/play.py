d1d45741523061308096ba827129ea46cca590a4
babloo-n <121771549+babloo-n@users.noreply.github.com>
2023-02-22 00:59:23 +0200

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -94,11 +94,11 @@ async def play_commnd(
     url,
     fplay,
 ):
-   if not await check_is_joined(message):
+    if not await check_is_joined(message):
         return
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
-    )
+    ) 
     plist_id = None
     slider = None
     plist_type = None
